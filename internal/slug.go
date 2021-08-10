8fc213643b12691181b6f7d50c9c7f54ac7ceea1
Paul Ebose <paulebose@gmail.com>
2021-08-10 04:10:42 +0100

    support wider range of letters in slug helper

--- internal/slug.go
+++ internal/slug.go
@@ -6,29 +6,24 @@ import (
 	"golang.org/x/text/unicode/norm"
 )
 
-var (
-	// The "safe" set of characters.
-	alphanum = &unicode.RangeTable{
-		R16: []unicode.Range16{
-			{0x0030, 0x0039, 1}, // 0-9
-			{0x0041, 0x005A, 1}, // A-Z
-			{0x0061, 0x007A, 1}, // a-z
-		},
-	}
-	// Characters in these ranges will be ignored.
-	nop = []*unicode.RangeTable{
-		unicode.Mark,
-		unicode.Sk, // Symbol - modifier
-		unicode.Lm, // Letter - modifier
-		unicode.Cc, // Other - control
-		unicode.Cf, // Other - format
-	}
-)
+// Characters in these ranges will be ignored.
+var nop = []*unicode.RangeTable{
+	unicode.Mark,
+	unicode.Sk, // Symbol - modifier
+	unicode.Lm, // Letter - modifier
+	unicode.Cc, // Other - control
+	unicode.Cf, // Other - format
+}
 
-// slug replaces each run of characters which are not ASCII letters or numbers
-// with the Replacement character, except for leading or trailing runs. Letters
-// will be stripped of diacritical marks and lowercased. Letter or number
-// codepoints that do not have combining marks or a lower-cased variant will be
+// slug transforms:
+// Æther -> æther
+// Mørdag -> mørdag
+// Être en été est à mi-chemin de noël -> etre-en-ete-est-a-mi-chemin-de-noel
+
+// slug replaces each run of characters which are not ASCII letters with
+// the '-' character, except for leading or trailing runs. Letters will
+// be stripped of diacritical marks and lowercased. Letter codepoints
+// that do not have combining marks or a lower-cased variant will be
 // passed through unaltered.
 func slug(s string) string {
 	buf := make([]rune, 0, len(s))
@@ -36,7 +31,7 @@ func slug(s string) string {
 
 	for _, r := range norm.NFKD.String(s) {
 		switch {
-		case unicode.In(r, alphanum):
+		case unicode.In(r, unicode.Letter):
 			buf = append(buf, unicode.ToLower(r))
 			replacement = true
 		case unicode.IsOneOf(nop, r):
